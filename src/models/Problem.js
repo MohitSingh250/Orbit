a5497ac6fbdb01a740038e677fbbad7ae4f988c1
Mohit Singh <mscrick01@gmail.com>
2025-11-04 18:59:46 +0530

    update problem schema for making contests part

--- src/models/Problem.js
+++ src/models/Problem.js
@@ -18,8 +18,9 @@ hints: [{ level: Number, text: String }],
 similarProblems: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Problem' }],
 solution: { type: String },
 createdAt: { type: Date, default: Date.now },
-contestExclusive: {type:Boolean},
-contestId:{type:mongoose.Schema.Types.ObjectId,ref:"Contest"}
+contestExclusive: { type: Boolean, default: false },
+contestId: { type: mongoose.Schema.Types.ObjectId, ref: "Contest", required: function() { return this.contestExclusive; } },
+
 });
 
 ProblemSchema.index({ title: 'text', statement: 'text' });
